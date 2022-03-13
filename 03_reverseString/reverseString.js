cbe953d6d56733fe42f81f80ccffd2e1fbdfd1af
jamesdarrelbautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2022-03-13 21:29:35 +0800

    done 2nd test and started 3rd test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverse = '';
+    let array = text.split("");
+    for (let index = array.length; index => 0; index--) {
+        reverse += array[index];
+    }
+    return console.log(reverse);
 };
 
 // Do not edit below this line
