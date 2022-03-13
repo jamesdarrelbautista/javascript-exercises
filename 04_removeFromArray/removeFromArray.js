1b3267be3e8bd223b0b5eaba7d1a3386f594d1c3
jamesdarrelbautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2022-03-14 00:08:32 +0800

    done with test 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...number) {
+    for (let index = 0; index < number.length; index++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j] == number[index] && typeof array[j] == typeof number[index]){
+                array.splice(j, 1);   
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (number1,number2) {
+    let min = number1
+    ,   max = number2;
+    if(number1>number2){
+        min = number2;
+        max = number1;
+    }
+    let sum = 0;
+    for (let index = min; index <= max; index++) {   
+        if(index < 0){
+            return 'ERROR';
+        } else if (typeof number1 != 'number' || typeof number2 != 'number'){
+            return 'ERROR';
+        }
+        sum += index;
+    }
+    if (Array.isArray(number1) || Array.isArray(number2)) return 'ERROR';
+    return sum;
 };
 
 // Do not edit below this line
