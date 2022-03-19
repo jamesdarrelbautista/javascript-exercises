a9e5a47b736b95244433d80d5ce3ff6b05b7848d
jamesdarrelbautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2022-03-20 00:41:10 +0800

    done 9 and 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    processedString = words.toLowerCase().replace(/[^a-z]/g, "");
+    return (processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if(number < 0) return "OOPS";
+    else if(typeof number == "string") parseInt(number);
+    
+    num1 = 0,num2 = 1, next_term = 0;
+    for (let index = 0; index < number; index++) {
+        if (index <= 1) next_term = 1;
+        next_term = num1 + num2 ;
+        num1 = num2;
+        num2 = next_term;
+    }
+    return num1;
 };
 
 // Do not edit below this line
