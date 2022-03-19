ae8352ab93df9ba65b83cad4a1b07cc7e18007b0
jamesdarrelbautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2022-03-20 01:01:36 +0800

    stuck in 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.reduce((previous, current) => {
+        previousAge = previous.yearOfDeath - previous.yearOfBirth;
+        currentAge = current.yearOfDeath - current.yearOfBirth;
+        return previousAge > currentAge ? previous : current;
+    })
 };
 
 // Do not edit below this line
